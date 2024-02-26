8b82260c1d4b43e2d26ef6cba9e6f2120f562a6c
yass50023 <yb9408316@gmail.com>
2024-02-26 13:10:52 +0100

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(NULL)); /* Seed the random number generator */
-	n = rand() - RAND_MAX / 2; /* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
+srand(time(NULL)); /* Seed the random number generator */
+n = rand() - RAND_MAX / 2; /* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
 
-	printf("%d ", n);
+printf("%d ", n);
 
-	if (n > 0)
-		printf("is positive\n");
-	else if (n == 0)
-		printf("is zero\n");
-	else
-		printf("is negative\n");
+if (n > 0)
+printf("is positive\n");
+else if (n == 0)
+printf("is zero\n");
+else
+printf("is negative\n");
 
-	return (0);
+return (0);
 }
