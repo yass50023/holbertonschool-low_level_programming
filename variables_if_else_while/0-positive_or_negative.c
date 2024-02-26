37d8514431bff38e3bfff88c48589eb4519a0237
yass50023 <yb9408316@gmail.com>
2024-02-26 11:29:07 +0100

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * This function generates a random number and prints whether
- * the number is positive, negative, or zero.
- *
- * Return: Always 0 (indicating success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    srand(time(NULL)); /* Seed the random number generator */
-    int n = rand() - RAND_MAX / 2; /* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
+    int n;
+
+    srand(time(NULL));
+    n = rand() - RAND_MAX / 2;
     
-    printf("The number %d ", n);
-    
+    printf("%d ", n);
+
     if (n > 0)
-    {
         printf("is positive\n");
-    }
     else if (n == 0)
-    {
         printf("is zero\n");
-    }
     else
-    {
         printf("is negative\n");
-    }
-    
-    return 0;
+
+    return (0);
 }
