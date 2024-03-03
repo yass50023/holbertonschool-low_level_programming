10d5ae587a681770306899daf993fedfbc055890
yass50023 <yb9408316@gmail.com>
2024-03-03 16:55:28 +0100

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_rev - prints a string, in reverse.
+ *@s: the string
+ *Return: void
+ */
+void print_rev(char *s)
+
+{
+	{
+		int i, j, L;
+
+		i = 0;
+
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+
+		L = i;
+
+		for (j = L - 1; j >= 0; j--)
+		{
+			_putchar(s[j]);
+		}
+
+		_putchar('\n');
+	}
+}
