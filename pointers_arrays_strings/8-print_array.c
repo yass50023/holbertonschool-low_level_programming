4613faa1067248056b94779b27160824b2c636ea
yass50023 <yb9408316@gmail.com>
2024-03-03 17:10:44 +0100

    commit

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,26 +1,39 @@
 #include "main.h"
+#include <unistd.h> // for write function
 
 /**
- *puts_half - prints half of a string.
- *@str: the string.
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * puts_half - prints half of a string.
+ * @str: the string.
  */
 void puts_half(char *str)
 {
-	int i, n;
+    int i, n;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+    i = 0;
+    while (str[i] != '\0')
+        i++;
 
-	n = i / 2;
+    n = i / 2;
 
-	if (i % 2 == 1)
-		n++;
+    if (i % 2 == 1)
+        n++;
 
-	while (n < i)
-	{
-		_putchar(str[n]);
-		n++;
-	}
-	_putchar('\n');
+    while (n < i)
+    {
+        _putchar(str[n]);
+        n++;
+    }
+    _putchar('\n');
 }
