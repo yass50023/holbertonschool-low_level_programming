532231d7caf9d47ada884d1ad58bdc7c84b46b07
yass50023 <yb9408316@gmail.com>
2024-03-04 14:48:08 +0100

    commit

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,6 +2,7 @@
 
 /**
  * _memcpy - Copies n bytes from memory area src to memory area dest
+ *
  * @dest: Pointer to the destination memory area
  * @src: Pointer to the source memory area
  * @n: Number of bytes to copy
@@ -10,13 +11,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *dest_ptr = dest;
-	char *src_ptr = src;
+    char *dest_ptr = dest;
+    char *src_ptr = src;
 
-	for (unsigned int i = 0; i < n; i++)
-	{
-		*dest_ptr++ = *src_ptr++;
-	}
+    while (n--)
+        *dest_ptr++ = *src_ptr++;
 
-	return dest;
+    return dest;
 }
