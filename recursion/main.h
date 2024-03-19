e8b7c49986d0366b861437d4ea84c0e4095a3e8e
yass50023 <yb9408316@gmail.com>
2024-03-19 03:55:38 +0100

    commit

--- recursion/main.h
+++ recursion/main.h
@@ -1,12 +1,7 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
