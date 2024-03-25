34ee570a3d1156da6c518644e5334ff6008b57cb
yass50023 <yb9408316@gmail.com>
2024-03-25 21:56:54 +0100

    commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _abs - Computes integer.
+  * @c: The number to be computed.
+  *
+  * Return: //0
+  */
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
