c2eaae6a081f289ff4b9850324c596413fd66b12
yass50023 <yb9408316@gmail.com>
2024-03-25 21:50:29 +0100

    commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_sign - Prints the sign 
+  * @n: The number to be checked
+  *
+  * Return: 0
+  */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
