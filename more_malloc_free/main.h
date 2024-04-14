cbfa7f8e0a620955331cfa0ea4a2bdf85d85f46b
yass50023 <yb9408316@gmail.com>
2024-04-14 19:45:04 +0200

    commit

---
+++ more_malloc_free/main.h
@@ -0,0 +1,5 @@
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *a, char *b, unsigned int n);
+int _strlen(char *s);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
