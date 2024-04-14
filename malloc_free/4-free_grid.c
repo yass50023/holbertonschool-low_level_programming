870caf89220e51c1ea00663ec6bdfaaec18d8d6e
yass50023 <yb9408316@gmail.com>
2024-04-14 17:12:12 +0200

    commit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees 2-D array
+ * @grid: 2-D array
+ * @height: Dimensions of array
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; ++i)
+		free(*(grid + i));
+	free(grid);
+}
