4fc438d665fbef2e84ad7d60a51e843810eb0b90
yass50023 <yb9408316@gmail.com>
2024-04-14 17:18:29 +0200

    commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char L;
+
+	for (L = 'a'; L <= 'z'; L++)
+		putchar(L);
+	putchar('\n');
+	return (0);
+}
