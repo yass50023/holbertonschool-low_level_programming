1b9edf31c7a515e3ac7db783938a3f39e11f4efb
yass50023 <yb9408316@gmail.com>
2024-04-14 17:02:58 +0200

    commit

---
+++ malloc_free/main.h
@@ -0,0 +1,5 @@
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *x, char *y);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
