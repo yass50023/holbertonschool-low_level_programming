3b4b852bf15c81470919ec7b289e03c6340ddbf5
yass50023 <yb9408316@gmail.com>
2024-04-14 16:35:01 +0200

    commit

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,56 +1,27 @@
+#include "main.h"
 
+/**
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-User
-Write a function that encodes a string into 1337.
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (str[indx1] == leet[indx2] ||
+			    str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
 
-Letters a and A should be replaced by 4
-Letters e and E should be replaced by 3
-Letters o and O should be replaced by 0
-Letters t and T should be replaced by 7
-Letters l and L should be replaced by 1
-Prototype: char *leet(char *);
-You can only use one if in your code
-You can only use two loops in your code
-You are not allowed to use switch
-You are not allowed to use any ternary operation
-ChatGPT
-Here's a C function that encodes a string into 1337 according to the given rules:
+		indx1++;
+	}
 
-c
-Copy code
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char *leet(char *str) {
-    char *encoded = strdup(str); // Create a duplicate of the input string
-    
-    for (int i = 0; str[i] != '\0'; i++) {
-        // Using only one if statement to check for each character
-        if (str[i] == 'a' || str[i] == 'A') {
-            encoded[i] = '4';
-        }
-        if (str[i] == 'e' || str[i] == 'E') {
-            encoded[i] = '3';
-        }
-        if (str[i] == 'o' || str[i] == 'O') {
-            encoded[i] = '0';
-        }
-        if (str[i] == 't' || str[i] == 'T') {
-            encoded[i] = '7';
-        }
-        if (str[i] == 'l' || str[i] == 'L') {
-            encoded[i] = '1';
-        }
-    }
-
-    return encoded;
-}
-
-int main() {
-    char str[] = "Hello, World!"; // Example input string
-    char *encoded = leet(str);
-    printf("Encoded string: %s\n", encoded);
-    free(encoded); // Free the memory allocated for the encoded string
-    return 0;
+	return (str);
 }
